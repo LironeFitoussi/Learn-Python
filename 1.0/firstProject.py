588b9db51a9487c62c6ff6c216fd6d3821cf33d3
LironeFitoussi <lironefit@icloud.com>
2023-09-11 00:21:07 +0300

    Upload

---
+++ 1.0/firstProject.py
@@ -0,0 +1,38 @@
+print("Hello World!");
+print(60);
+
+city = "Katmandu";
+print(city)
+print("city")
+
+city = "New York"
+print(city)
+
+city = "Katmandu"
+
+destination_city = "New York"
+
+city = destination_city
+
+print(city)
+
+# userName = input("Type User Name Here:")
+# print("Hello", userName, "!")
+
+my_favorite_number = 5
+print(my_favorite_number)
+
+pi = 3.14
+print(pi)
+
+my_favorite_number = pi
+print(my_favorite_number)
+
+print("City:", "Katmandu")
+
+city = "Katmandu"
+kfc_locations = 3
+print("City:", city, "KFC Locations:", kfc_locations)
+
+
+
