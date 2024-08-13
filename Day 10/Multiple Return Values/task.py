74192c1994d898c9f1e781d8603c3abdcc3965b0
LironeFitoussi <lironefit@gmail.com>
2024-08-13 15:33:16 +0300

    Finished day 10

--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -8,3 +8,14 @@ def format_name(f_name, l_name):
 
 
 print(format_name(input("What is your first name?: "), input("What is your last name?: ")))
+
+
+def add(n1 , n2):
+    return n1 + n2
+
+my_favorite_number = add 
+"""
+my_favorite_number is now a reference to the
+add function and not trying to call it 
+because there are no parentheses
+"""
