e4af22dffa1b06f95f10477526c23a29989b76e1
LironeFitoussi <lironefit@gmail.com>
2024-08-14 23:45:49 +0300

    lesson 87

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -8,3 +8,23 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+
+# drink_potion()
+# print(
+#     potion_strength
+# )  # This will raise an error because potion_strength is not defined outside the function. It is a local variable.
+
+
+player_health = 10
+
+
+def game():
+    def drink_potion():
+        # This is a local variable and it will not affect the global variable.
+        potion_strength = 2
+        print(player_health)
+
+
+drink_potion()  # This will return an error because potion_strength is not defined outside the function.
+print(player_health)
